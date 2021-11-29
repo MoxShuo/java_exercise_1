9a3d38a7f49a5c83fe6ba4d4aa14ede4000bc659
Jeremy Vissault <vissault@et.esiea.fr>
2021-11-29 15:48:00 +0100

    Part 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -6,13 +6,35 @@ public class Launcher { // (1)
         System.out.print("Ecrire la commande: ");
         String scan = scanner.next();
         String  quit_variable = "quit";
-        while(!scan.equals(quit_variable))
+        String fibo_variable = "fibo";
+        while(!scan.equals(quit_variable) && !scan.equals(fibo_variable))
         {
             System.out.println("Unknown Command");
             System.out.print("Ecrire la commande: ");
             scan = scanner.next();
 
         }
-
+        if(scan.equals(fibo_variable))
+        {
+            System.out.print("Quel est ton nombre: ");
+            int nombre = scanner.nextInt();
+            int i = 0;
+            int fibo = 0;
+            if(nombre == 0)
+            {
+                System.out.print("0");
+            }
+            if(nombre == 1)
+            {
+                System.out.print("1");
+            }
+            else if(nombre != 0 || nombre != 1)
+            {
+                for(i = 0;i <= nombre; i++) {
+                    fibo +=i;
+                }
+                System.out.print(fibo);
+            }
+        }
     }
 }
